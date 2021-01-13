391b6901c51c228fe0109c51432f9a6def0a85c0
zzhilin <zhilin_li@outlook.com>
2021-01-13 15:58:57 -0800

    added js

--- server.js
+++ server.js
@@ -27,5 +27,5 @@ app.use(express.static(__dirname + '/static'));
 // Start the server
 var port = process.env.PORT || PORT; // 80 for web, 3000 for development
 app.listen(port, function() {
-	console.log("Node.js server running on port %s", port);
+	console.log('Node.js server running on port %s', port);
 });
--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -1,9 +1,15 @@
 $(document).ready(function() {
-    initializePage();
+	initializePage();
 });
 
 function initializePage() {
-    $('#testjs').click(function(e) {
-            $('.jumbotron h1').text("Javascript is connected");
-            });
+	$('#testjs').click(function(e) {
+		$('.jumbotron h1').text('Javascript is connected');
+	});
+	$('a.thumbnail').click(projectClick);
+}
+
+function projectClick(e) {
+	e.preventDefault();
+	$(this).css('background-color', '#800');
 }
